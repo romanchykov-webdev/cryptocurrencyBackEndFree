ae0e0dae2f2b62fee755566f848e32c9de5b818b
faram <romanchykov.webdev@gmail.com>
2024-06-09 15:32:11 +0200

    get-public-user-info

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDTO } from '../user/dto';
 import { UserLoginDTO } from './dto';
 import { AuthUserResponse } from './response';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserService } from '../user/user.service';
+import { JwtAuthGuard } from '../../guards/jwt-guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {
+  }
 
   //registration user
   //to describer the documentation
@@ -32,6 +38,16 @@ export class AuthController {
   }
 
   //login user end------------------
+
+  // get public user info
+  @UseGuards(JwtAuthGuard)
+  @Get('get-public-user-info')
+  getPublicUserInfo(@Req() request) {
+    const user = request.user;
+    return this.userService.publicUser(user.email);
+  }
+
+  // get public user info end
 
   //@UseGuards(JwtAuthGuard)
   //@Post('test')
